54e641787ac11cac5106f706c8510191a2d6d289
iwa964 <inaw2006@icloud.com>
2025-07-17 17:52:26 -0400

    continue to work in lab

--- src/main/java/interface_adapter/gpt/PromptBuilder.java
+++ src/main/java/interface_adapter/gpt/PromptBuilder.java
@@ -1,10 +1,17 @@
 package interface_adapter.gpt;
 
 import entity.DailyLog;
+import entity.DailyTaskSummary;
 
 public class PromptBuilder {
     public static String buildPromptFromDailyLog(DailyLog log) {
         // idea: extract info: completed tasks, stress lvl, moods, overdues, etc
         // build coherent natural language prompt
+
+        StringBuilder sb = new StringBuilder();
+
+        int numCompletedTasks = log.getDailyTaskSummary().getCompletedTasks().size();
+        int num
+
     }
 }
