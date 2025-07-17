a92dd24fd8e451743f7558ca4cadd87705be83ca
Angela Su <angelasumas@gmail.com>
2025-07-17 06:10:38 -0400

    feat: add presenter for adding tasks to today

---
+++ src/main/java/interface_adapter/presenter/AddTaskToTodayPresenter.java
@@ -0,0 +1,27 @@
+package interface_adapter.presenter;
+
+import use_case.add_task_to_today.AddTaskToTodayOutputBoundary;
+import use_case.add_task_to_today.AddTaskToTodayOutputData;
+import view.TaskViewModel;
+
+/**
+ * Presenter for adding tasks to today.
+ */
+public class AddTaskToTodayPresenter implements AddTaskToTodayOutputBoundary{
+    private final TaskViewModel taskViewModel;
+
+    public AddTaskToTodayPresenter(TaskViewModel taskViewModel) {
+        this.taskViewModel = taskViewModel;
+    }
+
+    @Override
+    public void presentSuccess(AddTaskToTodayOutputData outputData) {
+        String message = String.format("'%s' added to today's tasks", outputData.getTaskName());
+        taskViewModel.setMessage(message);
+    }
+
+    @Override
+    public void presentError(String error) {
+        taskViewModel.setMessage("Error: " + error);
+    }
+}
