f0de3769dc0819238c8fe8a87cabe25ffe84eb73
Angela Su <angelasumas@gmail.com>
2025-07-17 02:54:19 -0400

    feat(interface-adapter): implement MarkTaskCompleteController for task completion

--- src/main/java/interface_adapter/MarkTaskCompleteController.java
+++ src/main/java/interface_adapter/MarkTaskCompleteController.java
@@ -1,4 +1,25 @@
 package interface_adapter;
 
+import use_case.MarkTaskCompleteUseCase;
+import use_case.MarkTaskCompleteInputData;
+
+/**
+ * Controller for marking tasks as complete.
+ */
 public class MarkTaskCompleteController {
+    private final MarkTaskCompleteUseCase markTaskCompleteUseCase;
+
+    public MarkTaskCompleteController(MarkTaskCompleteUseCase markTaskCompleteUseCase) {
+        this.markTaskCompleteUseCase = markTaskCompleteUseCase;
+    }
+
+    /**
+     * Marks a task as complete.
+     *
+     * @param taskId The ID of the task to complete
+     */
+    public void markTaskComplete(String taskId) {
+        MarkTaskCompleteInputData inputData = new MarkTaskCompleteInputData(taskId);
+        markTaskCompleteUseCase.execute(inputData);
+    }
 }
