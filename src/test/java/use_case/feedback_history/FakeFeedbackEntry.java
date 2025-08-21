e096e7c7f8e6704efba1626840ae2c5641e973e9
iwa964 <inaw2006@icloud.com>
2025-08-21 09:11:37 -0400

    FB history interactor test

---
+++ src/test/java/use_case/feedback_history/FakeFeedbackEntry.java
@@ -0,0 +1,24 @@
+package use_case.feedback_history;
+
+import entity.feedback_entry.FeedbackEntryInterf;
+import use_case.feedback_history.FeedbackHistoryOutputBoundary;
+import use_case.feedback_history.FeedbackHistoryOutputData;
+
+import java.time.LocalDate;
+
+record FakeFeedbackEntry(LocalDate date) implements FeedbackEntryInterf {
+    @Override
+    public String getAiAnalysis() {
+        return "";
+    }
+
+    @Override
+    public String getCorrelationData() {
+        return "";
+    }
+
+    @Override
+    public String getRecommendations() {
+        return "";
+    }
+}
